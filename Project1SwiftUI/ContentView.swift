a7825865823573da8b1ab10b13496718f4f8f3c7
Nicolas Lépinay <nicolaslepinay@outlook.fr>
2023-05-07 18:18:24 +0200

    Finished card widget

--- Project1SwiftUI/ContentView.swift
+++ Project1SwiftUI/ContentView.swift
@@ -2,13 +2,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("falls")
-                .resizable()
+
+        ZStack {
+            Color(.systemMint).ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Image("falls")
+                    .resizable()
+                    .cornerRadius(30)
+                    .aspectRatio(contentMode: .fit)
+                
+                HStack {
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                        }
+                    
+                        Text("361 reviews")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                }
+                Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
                 .cornerRadius(30)
-                .aspectRatio(contentMode: .fit)
-                .padding(.all)
-            Text("Niagara Falls")
+                .shadow(radius: 10)
+            )
+            .padding()
         }
         
     }
