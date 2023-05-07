005c83f3d25ef9ab0b21ce5cb97584a3793843b6
Nicolas Lépinay <nicolaslepinay@outlook.fr>
2023-05-07 15:11:42 +0200

    Initial Commit

---
+++ Project1SwiftUI/Project1SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1SwiftUIApp.swift
+//  Project1SwiftUI
+//
+//  Created by Nicolas Lépinay on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project1SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
